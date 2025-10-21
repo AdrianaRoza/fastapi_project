1cfd1dae97af3efca7561997a1f849c14bb14aef
Adriana Roza <adriana.roza.aka@gmail.com>
2025-10-21 10:24:28 -0400

    feat(auth): adds authentication and user creation routes

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,26 @@
 from fastapi import APIRouter
+from models import User, db
+from sqlalchemy.orm import sessionmaker
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def auth():
+async def home():
     """
     Essa é uma rota padrão de autenticação do nosso sistema
     """
     return {"mensagem": "Você acessou a rota padrão autentição", "autenticado":False}
+
+@auth_router.post("/create_account")
+async def create_account(name: str, email:str, password:str):
+    Session = sessionmaker(bind=db)
+    session = Session()
+    user = session.query(User).filter(User.email==email).fist()
+    if user:
+        return{"mensagem":"Já existe um usuário com esse email"}
+    
+    else:
+        new_user = User(name, email, password)
+        session.add(new_user)
+        session.commit()
+        return{"mensagem": "Usuáio cadastrado com sucesso"}
