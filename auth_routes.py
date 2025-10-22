5da42932b2d5806cec2bc123eda26d98b041f4c9
Adriana Roza <adriana.roza.aka@gmail.com>
2025-10-22 08:40:01 -0400

    feat(auth): adds error handling with HTTPException in user registration

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import capture_session
 from main import bcrypt_context
@@ -16,11 +16,11 @@ async def home():
 async def create_account(email: str, password: str, name: str, session = Depends(capture_session)):
     user = session.query(User).filter(User.email==email).first()
     if user:
-        return{"mensagem":"Já existe um usuário com esse email"}
+        raise HTTPException(status_code=400, detail="E-mail do usuário já cadastrado")
     
     else:
         encrypted_password = bcrypt_context.hash(password[:72])
         new_user = User(name=name, email=email, password=encrypted_password)
         session.add(new_user)
         session.commit()
-        return{"mensagem": "Usuáio cadastrado com sucesso"}
+        return{"mensagem": f"Usuáio cadastrado com sucesso {email}"}
