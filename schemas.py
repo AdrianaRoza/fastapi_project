2f52168635ceb59ee17124a4fb7160944da0d0e7
Adriana Roza <adriana.roza.aka@gmail.com>
2025-10-23 10:17:57 -0400

    refactor(schemas): standardize UserSchemas and add OrderSchemas

--- schemas.py
+++ schemas.py
@@ -1,12 +1,20 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class userSchemas(BaseModel):
+class UserSchemas(BaseModel):
     name: str
     email: str
     password: str
     active:Optional[bool]
     admin:Optional[bool]
+
+    class Config:
+        from_attributes = True
+
+
+
+class OrderSchemas(BaseModel):
+    user: int
 
     class Config:
         from_attributes = True
