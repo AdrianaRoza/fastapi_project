472052b835450d5b2e0f3a07c6a89939fad8cae3
Adriana Roza <adriana.roza.aka@gmail.com>
2025-10-23 10:14:08 -0400

    refactor(auth): standardize schema name to UserSchemas

--- auth_routes.py
+++ auth_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import capture_session
 from main import bcrypt_context
-from schemas import userSchemas
+from schemas import UserSchemas
 from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
@@ -15,7 +15,7 @@ async def home():
     return {"mensagem": "Você acessou a rota padrão autentição", "autenticado":False}
 
 @auth_router.post("/create_account")
-async def create_account(user_Schemas: userSchemas, session: Session = Depends(capture_session)):
+async def create_account(user_Schemas: UserSchemas, session: Session = Depends(capture_session)):
     user = session.query(User).filter(User.email==user_Schemas.email).first()
     if user:
         raise HTTPException(status_code=400, detail="E-mail do usuário já cadastrado")
