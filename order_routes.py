78c0bb0b08e77914eed50f6b8f6078ae5cb57da5
Adriana Roza <adriana.roza.aka@gmail.com>
2025-10-23 10:19:22 -0400

    feat(order): adds POST route for creating orders with OrderSchemas

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import capture_session
+from schemas import OrderSchemas
+from models import Order
 
 order_router = APIRouter(prefix="/order", tags=["order"])
 
@@ -8,3 +12,10 @@ async def order():
     Essa é uma rota padrão de pedidos do nosso sistema. Todas as rotas dos pedidos precisan de autenticação
     """
     return {"mensagem": "Você acessou a rota de pedidos"}
+
+@order_router.post("/order")
+async def create_order(order_schemas: OrderSchemas, session: Session = Depends(capture_session)):
+    new_order = Order(user=order_schemas.user)
+    session.add(new_order)
+    session.commit()
+    return{"mensagem": f"Pedido criado com sucesso. ID do pedido:{new_order.id}"}
