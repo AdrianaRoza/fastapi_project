964db1600a0c7a1a8fcbe6de707acd23db19991d
Adriana Roza <adriana.roza.aka@gmail.com>
2025-10-27 13:19:26 -0400

    feat(order): Adds authenticated order creation and cancellation routes with permission checking

--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from dependencies import capture_session
+from dependencies import capture_session, verify_token
 from schemas import OrderSchemas
-from models import Order
+from models import Order,User
 
-order_router = APIRouter(prefix="/order", tags=["order"])
+order_router = APIRouter(prefix="/order", tags=["order"], dependencies=[Depends(verify_token)])
 
 @order_router.get("/")
 async def order():
@@ -18,4 +18,18 @@ async def create_order(order_schemas: OrderSchemas, session: Session = Depends(c
     new_order = Order(user=order_schemas.user)
     session.add(new_order)
     session.commit()
-    return{"mensagem": f"Pedido criado com sucesso. ID do pedido:{new_order.id}"}
+    return{"mensagem": f"Pedido criado com sucesso. ID do pedido:{new_order.id}"} 
+
+@order_router.post("/order/cancelled/{order_id}")
+async def cancel_order(order_id: int,session: Session = Depends(capture_session), user: User = Depends(verify_token)):
+    order = session.query(Order).filter(Order.id==order_id).first()
+    if not order:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado")
+    if not user.admin and user.id != order.user:
+        raise HTTPException(status_code=401, detail="Você não tem autorização para fazer essa modificação")
+    order.status = "CANCELADO"
+    session.commit()
+    return{
+        "mensage": f"Pedido de número: {order_id} cancelado com sucesso",
+        "order":order
+}
