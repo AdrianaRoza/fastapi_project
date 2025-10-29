696d05ed3bc14be0e6a80952e4aa536e72c90dd9
Adriana Roza <adriana.roza.aka@gmail.com>
2025-10-29 09:17:09 -0400

    feat(order): adds authenticated routes for creating, canceling, and listing orders with permission verification.

--- order_routes.py
+++ order_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import capture_session, verify_token
 from schemas import OrderSchemas
-from models import Order,User
+from models import Order, User
 
 order_router = APIRouter(prefix="/order", tags=["order"], dependencies=[Depends(verify_token)])
 
@@ -21,7 +21,7 @@ async def create_order(order_schemas: OrderSchemas, session: Session = Depends(c
     return{"mensagem": f"Pedido criado com sucesso. ID do pedido:{new_order.id}"} 
 
 @order_router.post("/order/cancelled/{order_id}")
-async def cancel_order(order_id: int,session: Session = Depends(capture_session), user: User = Depends(verify_token)):
+async def cancel_order(order_id: int, session: Session = Depends(capture_session), user: User = Depends(verify_token)):
     order = session.query(Order).filter(Order.id==order_id).first()
     if not order:
         raise HTTPException(status_code=400, detail="Pedido não encontrado")
@@ -33,3 +33,14 @@ async def cancel_order(order_id: int,session: Session = Depends(capture_session)
         "mensage": f"Pedido de número: {order_id} cancelado com sucesso",
         "order":order
 }
+
+
+@order_router.get("/list")
+async def list_order(session: Session = Depends(capture_session), user: User = Depends(verify_token)):
+    if not user.admin:
+        raise HTTPException(status_code=401, detail="Você não tem autorização para fazer essa operação")
+    else: 
+        order = session.query(Order).all()
+        return{
+            "order": order
+        }
